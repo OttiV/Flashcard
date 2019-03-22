64dab3b5c029794ac0962ebcf1b849d7cab3e3eb
Vgnttv <o.vignani@gmail.com>
2019-03-22 11:32:29 +0100

    flashcard works

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 document.getElementById('flashcard1').addEventListener("click", displayAnswer);
-document.getElementById('flashcard1').addEventListener("click", displayQuestion);
+
+const flashcard = document.getElementById("flashcard1")
+
+function displayAnswer() {
+    flashcard.childNodes[1].textContent = 'Document Object Model'
+}
+    
+
 
 // function displayQuestion() {
 //     document.getElementById("flashcard1").innerHTML =
 //     "What is the DOM?";
 //   }
 
-function displayAnswer() {
-    document.getElementById("flashcard1").innerHTML ="Document Object Module";
-    // const flashcard = document.getElementById("flashcard1")
-    // const p = flashcard.createElement('p')
-    // p.innerHTML = "Document Object Module";
-    // flashcard.appendChild('p')
-  }
+
+// flashcard.onclick = function() {
+//     const question = document.getElementById("question1")
+//     const p = document.createElement('p')
+//     console.log(p)
+//     if (question === question.getAttribute('p')) {
+//         p.innerHTML = "Document Object Model"
+//         flashcard.appendChild(p)
+//     }
+// }
+
+// function displayAnswer() {
+    // document.getElementById("flashcard1").innerHTML ="Document Object Model";
+    // p.innerHTML = "Document Object Model";
+    // flashcard.appendChild(p)
+    // const p = document.createElement('p')
 
