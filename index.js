c1059c146337f5a47202cf8c8e7c6b35db22d13a
Vgnttv <o.vignani@gmail.com>
2019-03-22 11:04:02 +0100

    flashcard 1.0

---
+++ index.js
@@ -0,0 +1,16 @@
+document.getElementById('flashcard1').addEventListener("click", displayAnswer);
+document.getElementById('flashcard1').addEventListener("click", displayQuestion);
+
+// function displayQuestion() {
+//     document.getElementById("flashcard1").innerHTML =
+//     "What is the DOM?";
+//   }
+
+function displayAnswer() {
+    document.getElementById("flashcard1").innerHTML ="Document Object Module";
+    // const flashcard = document.getElementById("flashcard1")
+    // const p = flashcard.createElement('p')
+    // p.innerHTML = "Document Object Module";
+    // flashcard.appendChild('p')
+  }
+
